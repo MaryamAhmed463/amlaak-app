fe2a54d43a94b8ee3e8f36c4c15c0cde51de4e56
LENOVO <alriyamimeem5@gmail.com>
2020-10-31 20:56:40 +0400

    display driver profile_31-Oct

--- app/src/main/java/com/example/amlaakapp/view/fragment/ProfileDriverFragment.java
+++ app/src/main/java/com/example/amlaakapp/view/fragment/ProfileDriverFragment.java
@@ -1,14 +1,37 @@
 package com.example.amlaakapp.view.fragment;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.amlaakapp.MyProgressDialog;
 import com.example.amlaakapp.R;
+import com.example.amlaakapp.model.User;
+import com.example.amlaakapp.view.activity.LoginActivity;
+import com.example.amlaakapp.view.adapter.DisplayUsersAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Collections;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +48,11 @@ public class ProfileDriverFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    MyProgressDialog myProgressDialog;
+
+    private TextView txt_DCode , txt_DName , txt_DPhone , txt_DEmail;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     public ProfileDriverFragment() {
         // Required empty public constructor
@@ -61,7 +89,84 @@ public class ProfileDriverFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_driver, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_driver, container, false);
+
+        final Context mContext = getContext();
+        myProgressDialog=new MyProgressDialog(mContext);
+
+        TextView txtheader=view.findViewById(R.id.txt_header);
+        ImageView imgLogout = view.findViewById(R.id.img_logout);
+        txtheader.setText("Profile");
+        logout();
+        imgLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(mContext);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                dialog.setContentView(R.layout.custom_dialoge_exit);
+                Button btnYes = dialog.findViewById(R.id.btn_yes);
+                Button btnNo = dialog.findViewById(R.id.btn_no);
+                btnYes.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        FirebaseAuth.getInstance().signOut();
+                    }
+                });
+                btnNo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
+
+        txt_DCode = view.findViewById(R.id.txt_DCode);
+        txt_DName = view.findViewById(R.id.txt_DName);
+        txt_DPhone = view.findViewById(R.id.txt_DPhone);
+        txt_DEmail = view.findViewById(R.id.txt_DEmail);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference userDBRef = database.getReference("Users"+ "/" + FirebaseAuth.getInstance().getCurrentUser().getUid());
+        userDBRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                txt_DCode.setText(user.getsUserCode());
+                txt_DName.setText(user.getsFName()+" "+user.getsSName()+" "+user.getsLName());
+                txt_DPhone.setText(user.getsPhone());
+                txt_DEmail.setText(user.getsEmail());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        return view;
+    }
+
+    private void logout(){
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if(firebaseUser != null){
+                    //Log.d(TAG,"on auth state changed",firebaseUser.getUid());
+                }
+                else{
+                    Toast.makeText(getContext(),"You have logout",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+
+                }
+            }
+        };
     }
 
     public interface OnFragmentInteractionListener {
